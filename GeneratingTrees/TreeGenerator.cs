dff72f23ab05314707d97414d91c19d7880ded69
hdarjus <hdarjus@gmail.com>
2015-03-22 18:28:42 +0100

    Algorithm P added

---
+++ GeneratingTrees/TreeGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratingTrees {
+    public static class TreeGenerator {
+        /*
+         * Nested parentheses in lexicographic order
+         */
+        public static ICollection<String> AlgorithmP ( int n ) {
+            if ( n < 0 )
+                throw new ArgumentOutOfRangeException ( "Non-negative number expected" );
+            List<String> result = new List<String> ();
+            if ( n == 1 ) {
+                result.Add ( "()" );
+            } else if ( n >= 2 ) {
+                StringBuilder a = new StringBuilder ();
+                int m = 2 * n - 1;
+                a.Append ( ')' );
+                for ( int i = 1; i <= 2*n; i++ ) {
+                    a.Append ( i % 2 == 0 ? ')' : '(' );
+                }
+                bool end = false;
+                while ( !end ) {
+                    result.Add ( a.ToString ( 1, 2 * n ) );
+                    a[m] = ')';
+                    if ( a[m - 1] == ')' ) {
+                        a[m - 1] = '(';
+                        m--;
+                    } else {
+                        int j = m - 1;
+                        int k = 2 * n - 1;
+                        while ( a[j] == '(' ) {
+                            a[j] = ')';
+                            a[k] = '(';
+                            j--; k -= 2;
+                        }
+                        if ( j == 0 )
+                            end = true;
+                        else {
+                            a[j] = '(';
+                            m = 2 * n - 1;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTests/TreeGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeneratingTrees;
+
+namespace UnitTests {
+    [TestClass]
+    public class TreeGeneratorTests {
+        [TestMethod]
+        public void AlgorithmP_0 () {
+            Assert.IsTrue ( TreeGenerator.AlgorithmP ( 0 ).Count == 0 );
+        }
+
+        [TestMethod, Timeout(100)]
+        public void AlgorithmP_1 () {
+            ICollection<String> result = TreeGenerator.AlgorithmP ( 1 );
+            Assert.IsTrue ( result.Count == 1 );
+            Assert.IsTrue ( result.Contains ( "()" ) );
+        }
+
+        [TestMethod]
+        public void AlgorithmP_4 () {
+            // From the book
+            ICollection<String> nestedPar4 = new HashSet<String> () {
+                "()()()()", "()()(())", "()(())()", "()(()())",
+                "()((()))", "(())()()", "(())(())", "(()())()",
+                "(()()())", "(()(()))", "((()))()", "((())())",
+                "((()()))", "(((())))"};
+            ICollection<String> result = TreeGenerator.AlgorithmP ( 4 );
+
+            Assert.IsTrue ( result.Count == nestedPar4.Count );
+            Assert.IsTrue ( nestedPar4.All<String> ( e => result.Contains ( e ) ) );
+        }
+    }
+}
