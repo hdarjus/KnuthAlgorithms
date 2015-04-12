9496444b04a8fa51576cadb0b39fe5ddbdb0c8a4
hdarjus <hdarjus@gmail.com>
2015-04-12 14:25:41 +0200

    files added

---
+++ GeneratingTrees/ForestRepresentations/LeftRight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratingTrees.ForestRepresentations {
+    public class LeftRight : Forest {
+        public List<Tuple<int,int>> Data { get; private set; }
+
+        public LeftRight ( List<int> left, List<int> right ) {
+            int length = Math.Max ( 0, Math.Min ( left.Count, right.Count ) );
+            Data = new List<Tuple<int, int>> ( length );
+            for ( int i = 0; i < length; i++ ) {
+                Data.Add ( new Tuple<int, int> ( left[i], right[i] ) );
+            }
+        }
+
+        public override string ToString () {
+            List<String> l = new List<String> ();
+            List<String> r = new List<String> ();
+            for ( int i = 0; i < Data.Count; i++ ) {
+                l.Add ( Data[i].Item1.ToString () );
+                r.Add ( Data[i].Item2.ToString () );
+            }
+            return '[' + String.Join ( ", ", l.ToArray () ) + "] [" + String.Join ( ", ", r.ToArray () ) + ']';
+        }
+    }
+}
---
+++ GeneratingTrees/ForestRepresentations/Nodes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratingTrees.ForestRepresentations {
+    public class Nodes : Forest {
+        public List<Tree> children { get; private set; }
+
+        public Nodes () {
+            children = new List<Tree> ();
+        }
+
+        public void addChild ( Tree c ) {
+            children.Add ( c );
+        }
+
+        private bool isIsomorphic ( Nodes other ) {
+            return children.Count == other.children.Count &&
+                children.Zip<Tree, Tree, bool> ( other.children, (a,b) => a.isIsomorphic(b) ).All<bool> ( b => b );
+        }
+
+        public new bool Equals ( Forest other ) {
+            if ( other == null )
+                throw new NullReferenceException ();
+            Nodes n = other is Nodes ? (Nodes)other : Conversion.Convert2Nodes ( other );
+            return this.isIsomorphic ( n );
+        }
+
+        public override string ToString () {
+            return String.Join ( "", children.Select<Tree, String> ( t => t.ToString () ) );
+        }
+    }
+}
