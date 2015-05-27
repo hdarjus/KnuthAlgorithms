82d3eb839d94adb0cca1c820fd0e076e1e0320c3
hdarjus <hdarjus@gmail.com>
2015-05-27 21:59:06 +0200

    changes

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GeneratingTrees;
+using GeneratingTrees.ForestRepresentations;
 
 namespace GUI {
     /// <summary>
@@ -20,6 +23,38 @@ namespace GUI {
     public partial class MainWindow: Window {
         public MainWindow () {
             InitializeComponent ();
+
+            List<Parentheses> pars = (new List<String> () {
+                "", "()", "()()", "(())", "(()())",
+                "((()))()", "()(())"
+            }).Select<String, Parentheses>(s => new Parentheses (s)).ToList();
+            List<List<int>> lefts = new List<List<int>> () {
+                new List<int> (),
+                new List<int> () {0},
+                new List<int> () {0, 0},
+                new List<int> () {2, 0},
+                new List<int> () {2, 0, 0},
+                new List<int> () {2, 3, 0, 0},
+                new List<int> () {0, 3, 0}
+            };
+            List<List<int>> rights = new List<List<int>> () {
+                new List<int> (),
+                new List<int> () {0},
+                new List<int> () {2, 0},
+                new List<int> () {0, 0},
+                new List<int> () {0, 3, 0},
+                new List<int> () {4, 0, 0, 0},
+                new List<int> () {2, 0, 0}
+            };
+            TableViewModel tvm = new TableViewModel ();
+            for ( int i = 0; i < lefts.Count; i++ ) {
+                TableRow row = new TableRow ();
+                row.Row.Add ( pars[i] );
+                row.Row.Add ( new LeftRight ( lefts[i], rights[i] ) );
+                tvm.ResultTable.Add(row);
+            }
+
+            DataContext = tvm;
         }
     }
 }
---
+++ GUI/TableViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneratingTrees;
+using GeneratingTrees.ForestRepresentations;
+
+namespace GUI {
+    public class TableViewModel {
+        public ObservableCollection<TableRow> ResultTable { get; set; }
+
+        public TableViewModel () {
+            ResultTable = new ObservableCollection<TableRow> ();
+        }
+    }
+
+    public class TableRow {
+        public ObservableCollection<Forest> Row { get; set; }
+        public TableRow () {
+            Row = new ObservableCollection<Forest> ();
+        }
+    }
+}
