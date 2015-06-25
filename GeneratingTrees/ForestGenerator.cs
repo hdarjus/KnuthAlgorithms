f88c99a5774df150560ba4dd97bdb6805ed54cb0
Darjus Hosszejni <hdarjus@gmail.com>
2015-06-25 23:45:45 +0200

    algorithm n added

--- GeneratingTrees/ForestGenerator.cs
+++ GeneratingTrees/ForestGenerator.cs
@@ -14,36 +14,78 @@ namespace GeneratingTrees {
             if ( n < 0 )
                 throw new ArgumentOutOfRangeException ( "Non-negative number expected" );
             List<Forest> result = new List<Forest> ();
-            if ( n == 1 ) {
+            if ( n == 1 )
                 result.Add ( new Parentheses ( "()" ) );
-            } else if ( n >= 2 ) {
-                StringBuilder a = new StringBuilder ();
-                int m = 2 * n - 1;
-                a.Append ( ')' );
-                for ( int i = 1; i <= 2*n; i++ ) {
-                    a.Append ( i % 2 == 0 ? ')' : '(' );
+            if ( n <= 1 )
+                return result;
+            StringBuilder a = new StringBuilder ();
+            int m = 2 * n - 1;
+            a.Append ( ')' );
+            for ( int i = 1; i <= 2*n; i++ ) {
+                a.Append ( i % 2 == 0 ? ')' : '(' );
+            }
+            bool end = false;
+            while ( !end ) {
+                result.Add ( new Parentheses ( a.ToString ( 1, 2 * n ) ) );
+                a[m] = ')';
+                if ( a[m - 1] == ')' ) {
+                    a[m - 1] = '(';
+                    m--;
+                } else {
+                    int j = m - 1;
+                    int k = 2 * n - 1;
+                    while ( a[j] == '(' ) {
+                        a[j] = ')';
+                        a[k] = '(';
+                        j--; k -= 2;
+                    }
+                    if ( j == 0 )
+                        end = true;
+                    else {
+                        a[j] = '(';
+                        m = 2 * n - 1;
+                    }
                 }
-                bool end = false;
-                while ( !end ) {
-                    result.Add ( new Parentheses ( a.ToString ( 1, 2 * n ) ) );
-                    a[m] = ')';
-                    if ( a[m - 1] == ')' ) {
-                        a[m - 1] = '(';
-                        m--;
-                    } else {
-                        int j = m - 1;
-                        int k = 2 * n - 1;
-                        while ( a[j] == '(' ) {
-                            a[j] = ')';
-                            a[k] = '(';
-                            j--; k -= 2;
-                        }
-                        if ( j == 0 )
-                            end = true;
-                        else {
-                            a[j] = '(';
-                            m = 2 * n - 1;
-                        }
+            }
+            return result;
+        }
+
+        // Convert from list of 1 based left parenthesis indices to string of parentheses
+        private static String ZtoP ( List<int> z ) {
+            int n = z.Count;
+            StringBuilder res = new StringBuilder ( 2 * n );
+            for ( int i = 0; i < 2 * n; i++ )
+                res.Append ( ')' );
+            for ( int i = 0; i < n; i++ )
+                res[z[i]-1] = '(';
+            return res.ToString ();
+        }
+
+        public static List<Forest> AlgorithmN ( int n ) {
+            if ( n < 0 )
+                throw new ArgumentOutOfRangeException ( "Non-negative number expected" );
+            List<Forest> result = new List<Forest> ();
+            if ( n == 0 )
+                return result;
+            List<int> z = ( from j in Enumerable.Range ( 0, n ) select 2 * j + 1 ).ToList<int> ();
+            List<int> g = ( from j in Enumerable.Range ( 0, n ) select 2 * j ).ToList<int> ();
+            bool end = false;
+            while ( !end ) {
+                result.Add ( new Parentheses ( ZtoP ( z ) ) );
+                int j;
+                for ( j = n - 1; j >= 0 && g[j] == z[j]; j-- ) {
+                    g[j] = g[j] ^ 1;
+                }
+                if ( ( g[j] - z[j] ) % 2 == 0 ) {
+                    z[j] += 2;
+                } else {
+                    int t = z[j] - 2;
+                    if ( t < 0 )
+                        end = true;
+                    else {
+                        if ( t <= z[j - 1] )
+                            t += 1 + ( t < z[j - 1] ? 2 : 0 );
+                        z[j] = t;
                     }
                 }
             }
--- UnitTests/ForestGeneratorTest.cs
+++ UnitTests/ForestGeneratorTest.cs
@@ -8,6 +8,14 @@ using GeneratingTrees.ForestRepresentations;
 namespace UnitTests {
     [TestClass]
     public class ForestGeneratorTest {
+        // From the book
+        ICollection<String> nestedPar4 = new HashSet<String> () {
+            "()()()()", "()()(())", "()(())()", "()(()())",
+            "()((()))", "(())()()", "(())(())", "(()())()",
+            "(()()())", "(()(()))", "((()))()", "((())())",
+            "((()()))", "(((())))"
+        };
+
         [TestMethod]
         public void AlgorithmP_0 () {
             Assert.IsTrue ( ForestGenerator.AlgorithmP ( 0 ).Count == 0 );
@@ -22,14 +30,27 @@ namespace UnitTests {
 
         [TestMethod]
         public void AlgorithmP_4 () {
-            // From the book
-            ICollection<String> nestedPar4 = new HashSet<String> () {
-                "()()()()", "()()(())", "()(())()", "()(()())",
-                "()((()))", "(())()()", "(())(())", "(()())()",
-                "(()()())", "(()(()))", "((()))()", "((())())",
-                "((()()))", "(((())))"
-            };
             ICollection<Forest> result = ForestGenerator.AlgorithmP ( 4 );
+
+            Assert.IsTrue ( result.Count == nestedPar4.Count );
+            Assert.IsTrue ( nestedPar4.All<String> ( e => result.Contains ( new Parentheses ( e ) ) ) );
+        }
+        
+        [TestMethod]
+        public void AlgorithmN_0 () {
+            Assert.IsTrue ( ForestGenerator.AlgorithmN ( 0 ).Count == 0 );
+        }
+
+        [TestMethod, Timeout(100)]
+        public void AlgorithmN_1 () {
+            ICollection<Forest> result = ForestGenerator.AlgorithmN ( 1 );
+            Assert.IsTrue ( result.Count == 1 );
+            Assert.IsTrue ( result.Contains ( new Parentheses ( "()" ) ) );
+        }
+
+        [TestMethod]
+        public void AlgorithmN_4 () {
+            ICollection<Forest> result = ForestGenerator.AlgorithmN ( 4 );
 
             Assert.IsTrue ( result.Count == nestedPar4.Count );
             Assert.IsTrue ( nestedPar4.All<String> ( e => result.Contains ( new Parentheses ( e ) ) ) );
